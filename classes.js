af5411318a2274b655e77db478a2211b1d6edcc4
Nick Jacobson <33440907+nick-jac@users.noreply.github.com>
2017-12-12 16:09:40 +1300

    Added first changes to dom applying class

--- classes.js
+++ classes.js
@@ -5,20 +5,21 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
+  two()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
-  // First, we have to find the element:
   var one = document.getElementById('one')
-
-  // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
 
